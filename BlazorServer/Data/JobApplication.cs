b8a2c49f708e78d73ba5026ecb8a095f268989ab
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-12 19:41:27 -0400

    Creating Form with Validation

---
+++ BlazorServer/Data/JobApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Data
+{
+    public class JobApplication
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [StringLength(500, ErrorMessage ="You cannot put so many words here")]
+        public string Description { get; set; }
+        [Required]
+        [Range(10000, 150000, ErrorMessage ="Salary cannot be less than 1000 or bigger tha 150000")]
+        public int SalaryExpectation { get; set; }
+        [Required]
+        public bool DoesOpenSource { get; set; }
+        [Required]
+        public DateTime Availability { get; set; }
+    }
+}
