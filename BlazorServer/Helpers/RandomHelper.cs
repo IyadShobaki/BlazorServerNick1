ac1f04d70496ec8f165c73ade9fb8a8659988a4f
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-12 18:58:36 -0400

    JS Interop
    Calling C# methods from JavaScript

---
+++ BlazorServer/Helpers/RandomHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Helpers
+{
+    public class RandomHelper
+    {
+        [JSInvokable]
+        public Task<int> GenerateRandomInt(int maxValue)
+        {
+            return Task.FromResult(new Random().Next(maxValue));
+        }
+    }
+}
--- BlazorServer/wwwroot/js/interop.js
+++ BlazorServer/wwwroot/js/interop.js
@@ -12,3 +12,14 @@ function setElementTextById(id, text){
 function focusOnElement(element) {
     element.focus();
 }
+function giveMeRandomInt(maxIntSize, dotnetInstance) {
+    dotnetInstance.invokeMethodAsync('GenerateRandomInt', maxIntSize)
+        .then(result => {
+            setElementTextById('randomNumberSpan', result);
+        });
+
+    //DotNet.invokeMethodAsync('BlazorServer', 'GenerateRandomInt', maxIntSize)
+    //    .then(result => {
+    //        setElementTextById('randomNumberSpan', result);
+    //    });
+}
