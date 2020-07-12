9228aae266785e4788fa2ea4ce4ade7a196d7891
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-12 18:24:38 -0400

    JS Interop
    Calling JavaScript from C#

---
+++ BlazorServer/wwwroot/js/interop.js
@@ -0,0 +1,14 @@
+﻿function createAlert() {
+    alert("Hey this an alert");
+}
+
+function createPrompt(question) {
+    return prompt(question);
+}
+function setElementTextById(id, text){
+    document.getElementById(id).innerText = text;
+}
+
+function focusOnElement(element) {
+    element.focus();
+}
