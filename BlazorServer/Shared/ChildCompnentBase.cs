478f9d975826984ab2d35e50922fce1cca6ac398
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-12 11:19:06 -0400

    Initial commit

---
+++ BlazorServer/Shared/ChildCompnentBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Shared
+{
+    public class ChildCompnentBase : ComponentBase
+    {
+        protected bool DarkThemOn;
+        protected string AlertTheme => DarkThemOn ? "dark" : "light";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            DarkThemOn = true;
+        }
+    }
+}
